1b9ef91ab1f6a48053a41df0fa76c37ce804f0c5
Elena-aljebrin <elena.al-jebrin.etu@univ-lille.fr>
2023-10-20 08:49:15 +0200

    change3

--- project/code1.py
+++ project/code1.py
@@ -1,7 +1,17 @@
+"""
+This module defines a function to multiply two integers.
+"""
 def mult_two(a: int, b: int) -> int:
-    # your code here
-   result=a*b
-   return result
+    """
+    Multiply two integers and return the result.
+    Args:
+        a (int): The first integer.
+        b (int): The second integer.
+    Returns:
+        int: The result of multiplying a and b.
+    """
+    result=a*b
+    return result
 
 
 print("Example")
