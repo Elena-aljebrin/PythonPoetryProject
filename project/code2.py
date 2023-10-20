5edc2de6956d4d12dc9c4261d23187fe7227f964
Elena-aljebrin <elena.al-jebrin.etu@univ-lille.fr>
2023-10-20 08:54:22 +0200

    change3

--- project/code2.py
+++ project/code2.py
@@ -1,7 +1,14 @@
+"""
+This module defines a function to check if a number is even.
+"""
 def is_even(num: int) -> bool:
-    # your code here
-   return num%2 ==0
-
-
+    """
+    Check if a number is even.
+    Args:
+        num (int): The number to be checked.
+    Returns:
+    bool: True if the number is even, False otherwise.
+    """
+    return num%2 ==0
 print("Example:")
 print(is_even(2))
